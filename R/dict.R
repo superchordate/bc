dc6da83ebeeb28421841af424fa0534af4933406
Bryce Chamberlain <superchordate@gmail.com>
2019-02-15 19:39:42 -0600

    Fix dict, add list row for outlier rows and NAs for easy extraction

--- R/dict.R
+++ R/dict.R
@@ -24,12 +24,15 @@ dict = function( x, nastrings = bc::nastrings, run.on.sample = NULL, do.dropleve
       stringsAsFactors = FALSE
     )
 
+    # add row.
+    x$bc.row = 1:nrow(x)
+
     # sample rows first so values are consistent across.
-    splrows = sample( 1:nrow(x), size = min( 5, nrow(x) ), replace = FALSE )
+    splrows = sample( x$bc.row, size = min( 5, nrow(x) ), replace = FALSE )
     
     if( !is.null(run.on.sample) ) x = x[ sample( 1:nrow(x), size = ceiling( nrow(x) * run.on.sample ), replace = FALSE ), ]
 
-    for( col in colnames(x) ){
+    for( col in setdiff( colnames(x), 'bc.row' ) ){
 
         col.dict = data.frame( 
             name = col, 
@@ -57,22 +60,20 @@ dict = function( x, nastrings = bc::nastrings, run.on.sample = NULL, do.dropleve
         
         navals = which( is.na( x[[col]] ) )
         col.dict$unique = ifelse( !any( duplicated( x[[col]] ) ), 'unique', NA )
-        col.dict$na.ct = ifelse( length(navals) == 0, as.numeric(NA), length(navals) )
-        col.dict$na.pct = ifelse( length(navals) == 0, as.numeric(NA), round( length(navals) / nrow(x), 3 ) )
 
         if( is.factor( x[[col]] ) ){
           
           if( do.droplevels ) x[[col]] = droplevels( x[[col]] )
 
           if( length(navals) > 0 ) {
-            vals = x[[col]][ -navals ]
+            vals = x[ -navals, c( 'bc.row', col ) ]
           } else {
-            vals = x[[col]]
+            vals = x[ , c( 'bc.row', col ) ]
           }
           
-          valcnt = rev( sort( table( vals ) ) )
+          valcnt = rev( sort( table( vals[[col]] ) ) )
           col.dict$mode = as.character( names( valcnt )[1] )
-          col.dict$pareto80 = round( ( sum( cumsum(valcnt) < (length(vals) * 0.8) ) + 1 ) / length(valcnt), 2 )
+          col.dict$pareto80 = round( ( sum( cumsum(valcnt) < (nrow(vals) * 0.8) ) + 1 ) / length(valcnt), 2 )
 
           col.dict$n.vals = length( levels( x[[col]] ) )
             
@@ -81,45 +82,59 @@ dict = function( x, nastrings = bc::nastrings, run.on.sample = NULL, do.dropleve
         } else if( is.numeric( x[[col]] ) || lubridate::is.Date(x[[col]]) || is.POSIXct(x[[col]]) ){
 
             if( length(navals) > 0 ) {
-              vals = sort( x[[col]][ -navals ] )
+              vals = x[ -navals, c( 'bc.row', col ) ]
             } else {
-              vals = sort( x[[col]] )
+              vals = x[ , c( 'bc.row', col ) ]
             }
+          
+            vals = dplyr::arrange_at( vals, col )
 
-            pct25 = vals[ ceiling( length(vals) * .25 ) ]
-            pct50 = vals[ ceiling( length(vals) * .5 ) ]
-            pct75 = vals[ ceiling( length(vals) * .75 ) ]
-
-            col.dict$n.vals = length( unique( vals ) )
-            col.dict$min = as.character( vals[1] )
-            col.dict$pct25 = as.character( pct25 )
-            col.dict$pct50 = as.character( pct50 )
-            col.dict$pct75 = as.character( pct75 )
-            col.dict$max = as.character( vals[ length(vals) ] )
+            col.dict$n.vals = length( unique( vals[[col]] ) )
+            col.dict$min = vals[[col]][1]
+            col.dict$pct25 = vals[[col]][ ceiling( nrow(vals) * .25 ) ]
+            col.dict$pct50 = vals[[col]][ ceiling( nrow(vals) * .5 ) ]
+            col.dict$pct75 = vals[[col]][ ceiling( nrow(vals) * .75 ) ]
+            col.dict$max = vals[[col]][ nrow(vals) ]
             
             if( is.numeric( x[[col]] ) ){
-              iqr = pct75 - pct25
-              col.dict$num.outliers = sum( vals > pct75 + iqr )
-              col.dict$mean = as.character( signif( mean(vals), 3 ) )
-              col.dict$sd = as.character( signif( sd(vals), 3 ) )
+              
+              iqr = col.dict$pct75 - col.dict$pct25
+      
+              outlier.rows = vals$bc.row[ which( vals[[col]] > col.dict$pct75 + iqr  ) ]
+              col.dict$outlier.ct = length(outlier.rows)
+              col.dict$outlier.pct = col.dict$outlier.ct / nrow(vals)
+              if( length(outlier.rows) > 0 ) col.dict$outlier.rows = list(outlier.rows)
+
+              rm(outlier.rows)
+
+              col.dict$mean = mean(vals[[col]])
+              col.dict$sd = sd(vals[[col]])
             }
 
-            rm( vals, pct25, pct50, pct75 )
-
         } else if( is.logical( x[[col]] ) ) {
-          
+
           if( length(navals) > 0 ) {
-            vals = x[[col]][ -navals ]
+            vals = x[ -navals, c( 'bc.row', col ) ]
           } else {
-            vals = x[[col]]
+            vals = x[ , c( 'bc.row', col ) ]
           }
           
-          col.dict$mean = as.character( signif( mean(vals), 3 ) )
-          col.dict$sd = as.character( signif( sd(vals), 3 ) )
+          col.dict$mean = mean(vals[[col]])
+          col.dict$sd = sd(vals[[col]])
           
         }
+
+        col.dict$na.ct = length(navals)
+        col.dict$na.pct = length(navals) / nrow(vals)
+        if( length(navals) > 0 ) col.dict$na.rows = list( navals )
         
-        col.dict$sample = paste0( as.character( x[[col]][splrows] ), collapse = '; ' )
+        col.dict$sample = list( x[[col]][splrows] )
+
+        for( i in colnames(col.dict) ) if( is.numeric( col.dict[[i]] ) ) col.dict[[i]] = signif( col.dict[[i]], 3 )
+        rm(i)
+
+        for( i in colnames(col.dict) ) col.dict[[i]] = as.character(col.dict[[i]])
+        rm(i)
 
         dict = dplyr::bind_rows( dict, col.dict )
 
